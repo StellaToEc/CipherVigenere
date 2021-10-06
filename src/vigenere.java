5ce67f93ec70a7996442d6274c7e2bcfd7199883
stell <cristinatoes@gmail.com>
2021-10-06 13:20:40 -0500

    Put the filename without ".txt"

--- src/vigenere.java
+++ src/vigenere.java
@@ -1,4 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Locale;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.*;
+
+
 
 public class vigenere {
 
@@ -114,22 +127,76 @@ public class vigenere {
         return ot;
     }
 
+    static String fileMessage(String fileName) throws IOException {
+        String dirActual = "Text";
+        Path path = Paths.get("");
+        String directoryName = path.toAbsolutePath().toString();
 
-      public static void main (String[]args){
-          char abc[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ',',','.','-','\''};
+        String res = "";
+        String cadena;
+
+        directoryName+="\\"+dirActual+"\\"+fileName+".txt";
+
+
+        FileReader f = new FileReader(directoryName);
+        BufferedReader b = new BufferedReader(f);
+        while((cadena = b.readLine())!=null) {
+            res += cadena.toLowerCase(Locale.ROOT);
+        }
+
+        b.close();
+        return res;
+    }
+
+    static void saveText(String text,String fileName) {
+        String dirActual = "Text";
+        Path path = Paths.get("");
+        String directoryName = path.toAbsolutePath().toString();
+
+        directoryName+="\\"+dirActual+"\\"+fileName+"Edited.txt";
+
+        try {
+            File fileText = new File(directoryName);
+            FileWriter pw = new FileWriter(fileText, true);
+
+            pw.write(text);
+
+            pw.close();
+        }
+        catch (Exception e) {
+            System.out.println("Error writing text");
+        }
+    }
+
+
+      public static void main (String[]args) throws  IOException{
+          char abc[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ',',','.','\n','’'};
 
           Scanner reader = new Scanner(System.in);
-          System.out.println("Write the text:");
-          String pt = reader.nextLine();
+
+          System.out.println("Write the file:");
+          String namefile= reader.nextLine();
           System.out.println("Write the IV:");
-          String iv= reader.nextLine();
+          String iv = reader.nextLine();
           System.out.println("Write the key:");
-          String key= reader.nextLine();
+          String key = reader.nextLine();
+
+          if(iv.length()!=key.length()) {
+              do {
+                  System.out.println("Please try again the IV and the key will have the same size");
+                  System.out.println("Write the IV:");
+                  iv = reader.nextLine();
+                  System.out.println("Write the key:");
+                  key = reader.nextLine();
+              } while (iv.length() != key.length());
+          }
 
           System.out.println("to encrypt press 1 to decrypt 2");
           int elec= reader.nextInt();
 
-          System.out.println(modeCBCcipher(pt,iv,abc,key));
-          System.out.println(modeCBCdecipher(modeCBCcipher(pt,iv,abc,key),iv,abc,key));
+          switch (elec){
+              case 1-> saveText(modeCBCcipher(fileMessage(namefile),iv,abc,key),namefile);
+              case 2-> saveText(modeCBCdecipher(fileMessage(namefile),iv,abc,key),namefile);
+          }
       }
 }
